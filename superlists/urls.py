961d72c3722474e2bda32a8c1b63d2ac7e7d4055
sicheng1806 <sicheng1806@outlook.com>
2024-03-23 23:19:35 +0800

    first step of FT to styling

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path
-from django.conf.urls import include,url
+from django.conf.urls import include
 from lists import views
+from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$',views.home_page,name='home'),
-    re_path(r'^lists/new/?$',views.new_list,name='new_list'),
-    re_path(r'^lists/(\d+)/?$',views.view_list,name='view_list'),
-    re_path(r'^lists/(\d+)/add_item/?$',views.add_item,name='add_item'),
+    re_path(r'^$',views.home_page,name='home'),
+    re_path(r"^lists/",include(list_urls)),
     path('admin/', admin.site.urls),
 ]
