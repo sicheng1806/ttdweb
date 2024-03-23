37de1d0ef945afa648b2952610efe8bdbaa55c4a
sicheng1806 <sicheng1806@outlook.com>
2024-03-23 15:34:00 +0800

    Refactor functional test new function: check_for_rows_in_list_table

--- functional_test.py
+++ functional_test.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.brower.quit()
 
+    def check_for_rows_in_list_table(self,row_texts):
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name("tr")
+        for row_text in row_texts:
+            self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 卡秋纱听说又一个很酷的在线代办事项应用
         # 她去看了这个应用的首页
@@ -41,12 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n {table.text}"
-            )
+        self.check_for_rows_in_list_table(["1: Buy peacock feathers"])
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 "使用孔雀羽毛做假蝇"
         # 页面再次更新，她的清单中显示了这两个待办事项
@@ -54,17 +55,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n {table.text}"
-            )
-        self.assertTrue(
-            any(row.text == '2: Use peacock feathers to make a fly' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n {table.text}"
-            )
+        self.check_for_rows_in_list_table([
+            "1: Buy peacock feathers",
+            "2: Use peacock feathers to make a fly "
+        ])
         
         self.fail("Finish the test!")
 
