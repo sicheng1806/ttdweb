292afb33e5daf368eb5eb1509b464775a96679a4
sicheng1806 <sicheng1806@outlook.com>
2024-03-23 23:11:15 +0800

    first steps of FT for layout + styling

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include,url
 from lists import views
 
 urlpatterns = [
-    path(r'',views.home_page,name='home'),
+    url(r'^$',views.home_page,name='home'),
     re_path(r'^lists/new/?$',views.new_list,name='new_list'),
     re_path(r'^lists/(\d+)/?$',views.view_list,name='view_list'),
     re_path(r'^lists/(\d+)/add_item/?$',views.add_item,name='add_item'),
