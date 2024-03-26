041fa337a77afe82d449f0ce060a2d3a9c2fcb7a
sicheng1806 <sicheng1806@outlook.com>
2024-03-26 18:40:57 +0800

    create ExistingListItemForm to deel with DUPLICATE_ITME_ERROR

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from lists.models import Item
+
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -19,6 +22,18 @@ class ItemForm(forms.models.ModelForm):
             'text':{'required':EMPTY_ITEM_ERROR}
         }
     
-    def save(self,to_list,commit=True):
-        self.instance.list = to_list
+    def save(self,for_list,commit=True):
+        self.instance.list = for_list
         return super().save(commit=commit)
+    
+class ExistingListItemForm(ItemForm):
+    def __init__(self,for_list,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.instance.list = for_list
+    
+    def validate_unique(self) -> None:
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text':[DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm,EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm,ExistingListItemForm,
+    EMPTY_ITEM_ERROR,DUPLICATE_ITEM_ERROR
+    )
 from lists.models import List,Item
 
 class ItemFormTest(TestCase):
@@ -21,7 +24,31 @@ class ItemFormTest(TestCase):
     def test_form_save_handles_saving_to_a_list(self):
         list_ = List.objects.create()
         form = ItemForm(data={'text':'do me'})
-        new_item = form.save(to_list = list_)
+        new_item = form.save(for_list = list_)
         self.assertEqual(Item.objects.count(), 1)
         self.assertEqual(List.objects.count(),1)
         self.assertEqual(Item.objects.first().text,'do me')
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"',form.as_p())
+        self.assertIn('class="form-control input-lg"',form.as_p())
+    
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list = list_,data={"text":''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_ITEM_ERROR]
+        )
+    
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_,text='item1')
+        form = ExistingListItemForm(for_list = list_,data={'text':'item1'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'],[DUPLICATE_ITEM_ERROR])
